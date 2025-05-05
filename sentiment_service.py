a07edf8b0220e1bc1c88317b5194a9ee25a5617d
AMOQUIN <augustin.moquin@ynov.com>
2025-05-05 17:23:27 +0200

    swagger

--- sentiment_service.py
+++ sentiment_service.py
@@ -1,9 +1,35 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 
+# Initialize Swagger
+swagger = Swagger(app)
+
 @app.route('/analyze', methods=['POST'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'Sentiment analysis result',
+            'examples': {
+                'application/json': {
+                    'score': 0.5
+                }
+            }
+        }
+    },
+    'parameters': [
+        {
+            'name': 'text',
+            'in': 'body',
+            'type': 'string',
+            'required': True,
+            'description': 'Text to analyze'
+        }
+    ]
+})
 def analyze():
     data = request.get_json()
     text = data.get("text", "")
